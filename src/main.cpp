a1417ff9dc977f9ae59b73ca0b806ffa32117d26
schreibfaul1 <schreibfaul1@t-online.de>
2025-02-25 06:52:43 +0100

    Arduino 3.1.3

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,9 @@ void setup() {
     Serial.begin(115200);
     Serial.print("A\n\n");
     Serial.println("----------------------------------");
-    uint8_t avMajor  = ESP_ARDUINO_VERSION_MAJOR;
-    uint8_t avMinor  = ESP_ARDUINO_VERSION_MINOR;
-    uint8_t avPatch  = ESP_ARDUINO_VERSION_PATCH;
     Serial.printf("ESP32 Chip: %s\n", ESP.getChipModel());
-    Serial.printf("Arduino Version: %d.%d.%d\n", avMajor, avMinor, avPatch);
-    uint8_t idfMajor = ESP_IDF_VERSION_MAJOR;
-    uint8_t idfMinor = ESP_IDF_VERSION_MINOR;
-    uint8_t idfPatch = ESP_IDF_VERSION_PATCH;
-    Serial.printf("ESP-IDF Version: %d.%d.%d\n", idfMajor, idfMinor, idfPatch);
+    Serial.printf("Arduino Version: %d.%d.%d\n", ESP_ARDUINO_VERSION_MAJOR, ESP_ARDUINO_VERSION_MINOR, ESP_ARDUINO_VERSION_PATCH);
+    Serial.printf("ESP-IDF Version: %d.%d.%d\n", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR, ESP_IDF_VERSION_PATCH);
     Serial.printf("ARDUINO_LOOP_STACK_SIZE %d words (32 bit)\n", CONFIG_ARDUINO_LOOP_STACK_SIZE);
     Serial.println("----------------------------------");
     Serial.print("\n\n");
