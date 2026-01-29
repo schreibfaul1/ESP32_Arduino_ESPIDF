9b1161b1f393d7c7907377a843145ec9a14a3391
schreibfaul1 <schreibfaul1@t-online.de>
2026-01-29 07:51:12 +0100

    Arduino 3.3.6 IDF 5.5.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 #include "Arduino.h"
 #include "Audio.h"
-#include "WiFi.h"
 
 #ifdef CONFIG_IDF_TARGET_ESP32
 #define I2S_DOUT      25
 #define I2S_BCLK      27
 #define I2S_LRC       26
+#define SD_MMC_D0      2
+#define SD_MMC_CLK    14
+#define SD_MMC_CMD    15
 #endif
 
 #ifdef CONFIG_IDF_TARGET_ESP32S3
 #define I2S_DOUT      9
 #define I2S_BCLK      3
 #define I2S_LRC       1
+#define SD_MMC_D0    11
+#define SD_MMC_CLK   13
+#define SD_MMC_CMD   14
 #endif
 
 #ifdef CONFIG_IDF_TARGET_ESP32P4
 #define I2S_DOUT      22
 #define I2S_BCLK      20
 #define I2S_LRC       21
+#define SD_MMC_D0     11
+#define SD_MMC_CLK    13
+#define SD_MMC_CMD    14
 #endif
 
 String ssid =     "*****";
@@ -42,9 +50,13 @@ void setup() {
     Serial.print("\n\n");
     WiFi.begin(ssid.c_str(), password.c_str());
     while (WiFi.status() != WL_CONNECTED) delay(1500);
+    pinMode(SD_MMC_D0, INPUT_PULLUP);
+    SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+    SD_MMC.begin("/sdcard", true);
     audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
     audio.setVolume(20); // default 0...21
-    audio.connecttohost("http://stream.antennethueringen.de/live/aac-64/stream.antennethueringen.de/"); // aac
+  //  audio.connecttoFS(SD_MMC, myPlaylist[0].c_str());
+  //  audio.connecttohost("http://stream.antennethueringen.de/live/aac-64/stream.antennethueringen.de/"); // aac
   //  audio.connecttohost("http://stream.danubiusradio.hu:8091/danubius_HiFi"); // flac
   //  audio.connecttohost("http://stream.revma.ihrhls.com/zc4882/hls.m3u8");
 
